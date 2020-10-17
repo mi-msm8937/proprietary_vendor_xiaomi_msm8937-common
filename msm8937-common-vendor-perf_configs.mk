f62f2631269d7e74af11c770b88f01c9bc1e5fe8
zqm64 <zqm64@outlook.com>
2020-10-17 21:33:27 +0800

    msm8937-common: perf_configs: Update from ASUS X00PD Q

--- msm8937-common-vendor-perf_configs.mk
+++ msm8937-common-vendor-perf_configs.mk
@@ -2,5 +2,7 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/msm8937-common/perf_configs/vendor/etc/perf/commonresourceconfigs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/commonresourceconfigs.xml \
     vendor/xiaomi/msm8937-common/perf_configs/vendor/etc/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
     vendor/xiaomi/msm8937-common/perf_configs/vendor/etc/perf/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml \
+    vendor/xiaomi/msm8937-common/perf_configs/vendor/etc/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf \
+    vendor/xiaomi/msm8937-common/perf_configs/vendor/etc/perf/perf-profile1.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile1.conf \
     vendor/xiaomi/msm8937-common/perf_configs/vendor/etc/perf/targetconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/targetconfig.xml \
     vendor/xiaomi/msm8937-common/perf_configs/vendor/etc/perf/targetresourceconfigs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/targetresourceconfigs.xml
