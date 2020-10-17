2265d3750e6038e3103277eb7923a2024b869a74
zqm64 <zqm64@outlook.com>
2020-10-17 21:11:38 +0800

    msm8937-common: gpu_firmware: Update from ASUS X00PD Q

--- msm8937-common-vendor-gpu_firmware.mk
+++ msm8937-common-vendor-gpu_firmware.mk
@@ -1,7 +1,10 @@
 PRODUCT_COPY_FILES += \
+    vendor/xiaomi/msm8937-common/gpu_firmware/vendor/firmware/a300_pfp.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/a300_pfp.fw \
+    vendor/xiaomi/msm8937-common/gpu_firmware/vendor/firmware/a300_pm4.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/a300_pm4.fw \
     vendor/xiaomi/msm8937-common/gpu_firmware/vendor/firmware/a506_zap.b00:$(TARGET_COPY_OUT_VENDOR)/firmware/a506_zap.b00 \
     vendor/xiaomi/msm8937-common/gpu_firmware/vendor/firmware/a506_zap.b01:$(TARGET_COPY_OUT_VENDOR)/firmware/a506_zap.b01 \
     vendor/xiaomi/msm8937-common/gpu_firmware/vendor/firmware/a506_zap.b02:$(TARGET_COPY_OUT_VENDOR)/firmware/a506_zap.b02 \
+    vendor/xiaomi/msm8937-common/gpu_firmware/vendor/firmware/a506_zap.elf:$(TARGET_COPY_OUT_VENDOR)/firmware/a506_zap.elf \
     vendor/xiaomi/msm8937-common/gpu_firmware/vendor/firmware/a506_zap.mdt:$(TARGET_COPY_OUT_VENDOR)/firmware/a506_zap.mdt \
     vendor/xiaomi/msm8937-common/gpu_firmware/vendor/firmware/a530_pfp.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/a530_pfp.fw \
     vendor/xiaomi/msm8937-common/gpu_firmware/vendor/firmware/a530_pm4.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/a530_pm4.fw
