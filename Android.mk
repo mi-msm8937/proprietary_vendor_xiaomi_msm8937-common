ce9803d90abfaf98ff21ab9f088aa3c44b4ad63f
zqm64 <zqm64@outlook.com>
2020-11-03 16:48:30 +0800

    msm8937-common: riva => rova

--- Android.mk
+++ Android.mk
@@ -16,6 +16,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter ugg ugglite ulysse riva land santoni,$(TARGET_DEVICE)),)
+ifneq ($(filter ugg ugglite ulysse rova land santoni,$(TARGET_DEVICE)),)
 
 endif
