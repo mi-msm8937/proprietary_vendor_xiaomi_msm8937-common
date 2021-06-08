40d5b3b2dbcc46964d4c6a3d0945a7115bebbbda
zqm64 <zqm64@outlook.com>
2021-06-08 20:23:43 +0800

    msm8937-common: Update off-charger animation from olive Q

---
+++ msm8937-common-vendor-charger.mk
@@ -0,0 +1,23 @@
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/msm8937-common/charger/vendor/bin/chargeonlymode:$(TARGET_COPY_OUT_VENDOR)/bin/chargeonlymode \
+    vendor/xiaomi/msm8937-common/charger/vendor/lib64/libsuspend.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libsuspend.so \
+    vendor/xiaomi/msm8937-common/charger/vendor/res/charger/0.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/0.png \
+    vendor/xiaomi/msm8937-common/charger/vendor/res/charger/10.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/10.png \
+    vendor/xiaomi/msm8937-common/charger/vendor/res/charger/1.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/1.png \
+    vendor/xiaomi/msm8937-common/charger/vendor/res/charger/2.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/2.png \
+    vendor/xiaomi/msm8937-common/charger/vendor/res/charger/3.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/3.png \
+    vendor/xiaomi/msm8937-common/charger/vendor/res/charger/4.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/4.png \
+    vendor/xiaomi/msm8937-common/charger/vendor/res/charger/5.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/5.png \
+    vendor/xiaomi/msm8937-common/charger/vendor/res/charger/6.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/6.png \
+    vendor/xiaomi/msm8937-common/charger/vendor/res/charger/7.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/7.png \
+    vendor/xiaomi/msm8937-common/charger/vendor/res/charger/8.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/8.png \
+    vendor/xiaomi/msm8937-common/charger/vendor/res/charger/9.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/9.png \
+    vendor/xiaomi/msm8937-common/charger/vendor/res/charger/Battery_0.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/Battery_0.png \
+    vendor/xiaomi/msm8937-common/charger/vendor/res/charger/battery_fail.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/battery_fail.png \
+    vendor/xiaomi/msm8937-common/charger/vendor/res/charger/Battery_green1.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/Battery_green1.png \
+    vendor/xiaomi/msm8937-common/charger/vendor/res/charger/Battery_green2.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/Battery_green2.png \
+    vendor/xiaomi/msm8937-common/charger/vendor/res/charger/Battery_green3.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/Battery_green3.png \
+    vendor/xiaomi/msm8937-common/charger/vendor/res/charger/Battery.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/Battery.png \
+    vendor/xiaomi/msm8937-common/charger/vendor/res/charger/Battery_red1.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/Battery_red1.png \
+    vendor/xiaomi/msm8937-common/charger/vendor/res/charger/Battery_red2.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/Battery_red2.png \
+    vendor/xiaomi/msm8937-common/charger/vendor/res/charger/battery_scale.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/battery_scale.png
